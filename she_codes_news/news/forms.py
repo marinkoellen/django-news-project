d8cec9eaeb0bbf142ca3213492489d339fb18641
marinkoellen <ellen.marinko1@gmail.com>
2020-08-05 19:39:44 +0800

    log in for username

--- she_codes_news/news/forms.py
+++ she_codes_news/news/forms.py
@@ -15,20 +15,11 @@ class StoryForm(ModelForm):
 			# type time tells django to use the HTML5 time input
         )
     )
-# Model metadata is “anything that’s not a field”, such as ordering options (ordering), database table name (db_table), or human-readable singular and plural names (verbose_name and verbose_name_plural). None are required, and adding class Meta to a model is completely optional. 
-
-# A metaclass is the class of a class. A class defines how an instance of the class (i.e. an object) behaves while a metaclass defines how a class behaves. A class is an instance of a metaclass.
-
-# metaclasses therefore allow you to do 'extra things' when creating a class, like registering the new class with some registry or replace the class with something else entirely.
-
-#Meta inner class in Django models:
-
-# This is just a class container with some options (metadata) attached to the model. It defines such things as available permissions, associated database table name, whether the model is abstract or not, singular and plural versions of the name etc.
 
 
     class Meta:
         model = NewsStory
-        fields = ['title', 'author','pub_date','image_upload','content']
+        fields = ['title','pub_date','image_upload','content']
         widgets = {
             # 'pub_date': forms.DateInput(
             #     format=('%m/%d/%Y'),
@@ -51,3 +42,17 @@ class StoryForm(ModelForm):
         
 
         }
+
+
+# Model metadata is “anything that’s not a field”, such as ordering options (ordering), database table name (db_table), or human-readable singular and plural names (verbose_name and verbose_name_plural). None are required, and adding class Meta to a model is completely optional. 
+
+# A metaclass is the class of a class. A class defines how an instance of the class (i.e. an object) behaves while a metaclass defines how a class behaves. A class is an instance of a metaclass.
+
+# metaclasses therefore allow you to do 'extra things' when creating a class, like registering the new class with some registry or replace the class with something else entirely.
+
+#Meta inner class in Django models:
+
+# This is just a class container with some options (metadata) attached to the model. It defines such things as available permissions, associated database table name, whether the model is abstract or not, singular and plural versions of the name etc.
+
+
+#time_attrs={'type': 'time' PUT DEFAULTS AND CLASSES IN HERE EVERYTHING IN HERE},
